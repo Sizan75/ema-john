865f87530253e7ef4f29f14b78a674d2243910e6
Sizan75 <sakiuzzaman26@gmail.com>
2022-10-07 21:33:34 +0600

    new component added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
-import Header from './components/Header/Header';
-import Shop from './components/Shop/Shop';
+import Main from './layouts/Main/Main';
 
 function App() {
+  const router=createBrowserRouter([
+    {path: '/',
+    element:<Main></Main>
+    }
+  ])
   return (
     <div>
-      <Header></Header>
-      <Shop></Shop>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
